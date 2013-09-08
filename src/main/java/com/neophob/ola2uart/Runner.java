54875d16d200efaa8513558243d71175680bba16
Michael Vogt <michu@neophob.com>
2013-09-08 22:51:55 +0200

    minor cleanup

--- src/main/java/com/neophob/ola2uart/Runner.java
+++ src/main/java/com/neophob/ola2uart/Runner.java
@@ -1,7 +1,6 @@
 package com.neophob.ola2uart;
 
 import java.util.Map;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
 import ola.OlaClient;
@@ -10,16 +9,11 @@ import ola.proto.Ola.PluginListReply;
 import ola.proto.Ola.UniverseInfoReply;
 
 import com.neophob.ola2uart.config.Config;
-import com.neophob.ola2uart.log.MyFormatter;
 import com.neophob.ola2uart.ola.OlaHelper;
 import com.neophob.ola2uart.output.tpm2.Tpm2Protocol;
 import com.neophob.ola2uart.stat.StatisticHelper;
 
 /**
- * TODO: 
- * 	parameter
- * 	error handling (olad restart)
- *  finally
  *  
  * hint, do not use register for dmx:
  * 	The problem is that all the calls are blocking, so streaming updates 
@@ -37,14 +31,18 @@ import com.neophob.ola2uart.stat.StatisticHelper;
 public class Runner {
 
 	private static final String VERSION = "0.1";
-	
+
 	private static final long WARNING_NO_DMX_DATA_TIMEOUT = 60000;
 	private static final long STATISTIC_OUTPUT_TIMEOUT = 30000;
 
 	private static final Logger LOG = Logger.getLogger(Runner.class.getName());
-	 
+
 	private static Config cfg = null;
 	
+	private static long lastErrorMessage=0;
+	private static long lastDebugOutput=0;		
+	
+
 	/**
 	 * 
 	 * https://code.google.com/p/open-lighting/source/browse/java/src/test/java/ola/OlaClientTest.java#95
@@ -60,21 +58,17 @@ public class Runner {
 	 * 
 	 * -> parameter DMXUNIVERSE:PANEL_OFFSET
 	 * 
-	 * TODO fps definition...
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {		
-		long lastErrorMessage=0;
-		long lastDebugOutput=0;		
-
 		LOG.info("OLA-to-TPM2.net Daemon v"+VERSION+" by Michael Vogt / neophob.com");
 		LOG.info("Read DMX universe from OLA and send the as TPM2 packet to the serial port");
 
 		cfg = new Config(args);
 
 		OlaClient olaClient=OlaHelper.connectToOlad();
-			
+
 		PluginListReply replyPlugins = olaClient.getPlugins();        
 		LOG.finest(replyPlugins.toString());        
 
@@ -90,58 +84,17 @@ public class Runner {
 		}
 
 		Framerate framerate = new Framerate(cfg.getFps());
-		
+
 		LOG.info("enter main loop...");
 		while (true) {
-			
-			try {
-				int currentUniverse=0;				
-				for (Map.Entry<Integer,Integer> e: cfg.getDmxToOffsetMap().entrySet()) {
-					long t1 = System.currentTimeMillis();
-					DmxData reply = olaClient.getDmx(e.getKey());					
-					long t2 = System.currentTimeMillis()-t1;
-					short[] dmxData = olaClient.convertFromUnsigned(reply.getData());					
-					
-					if (cfg.isDebugOutput()) {
-						debugln("Time to get data from universe "+e.getKey()+": "+t2+"ms");
-					}
-					
-					if (dmxData.length>0) {
-						//send data via serial port
-						
-						debug(StatisticHelper.INSTANCE.getFrameCount()+" send "+dmxData.length+" bytes to universe "+currentUniverse);
-												
-						byte[] data = Tpm2Protocol.doProtocol(dmxData, currentUniverse, cfg.getDmxToOffsetMap().size());
-						cfg.getOutput().sendData(currentUniverse, data);
-						
-						StatisticHelper.INSTANCE.incrementAndGetPacketsRecieved();
-						StatisticHelper.INSTANCE.updateSendBytes(dmxData.length);
-						currentUniverse++;
 
-						debugln("... done");
-						if (System.currentTimeMillis()-lastDebugOutput > STATISTIC_OUTPUT_TIMEOUT) {
-							LOG.info(" sent "+StatisticHelper.INSTANCE.getPacketCount()+" packages ("+(StatisticHelper.INSTANCE.getSentBytes()/1024)+" kb), errors: "
-									+StatisticHelper.INSTANCE.getErrorCount());						
-							lastDebugOutput = System.currentTimeMillis();
-						}
-						
-					} else {
-						debugln("no data found for universe "+e.getKey());
-						if (System.currentTimeMillis()-lastErrorMessage > WARNING_NO_DMX_DATA_TIMEOUT) {
-							LOG.info("no dmx data for universe "+e.getKey());						
-							lastErrorMessage = System.currentTimeMillis();
-						}
-					}
-				}
-				
-				long cnt = StatisticHelper.INSTANCE.incrementAndGetFrameCount();
-				framerate.waitForFps(cnt);
-				
+			try {
+				eventLoop(olaClient, framerate);
 			} catch (NullPointerException e) {
 				// happens if olad is restarted (for example), wait and retry!
 				LOG.severe("NullPointer detected, sleep 1s and try to reconnect ...");	
 				Thread.sleep(1000);
-				
+
 				try {
 					olaClient = new OlaClient();
 				} catch (Exception e2) {
@@ -151,13 +104,68 @@ public class Runner {
 
 		}
 	}
-	
+
+	/**
+	 * 
+	 * @param olaClient
+	 * @param framerate
+	 */
+	private static void eventLoop(OlaClient olaClient, Framerate framerate) {
+		int currentUniverse=0;				
+		for (Map.Entry<Integer,Integer> e: cfg.getDmxToOffsetMap().entrySet()) {
+			long t1 = System.currentTimeMillis();
+			DmxData reply = olaClient.getDmx(e.getKey());					
+			long t2 = System.currentTimeMillis()-t1;
+			short[] dmxData = olaClient.convertFromUnsigned(reply.getData());					
+
+			if (cfg.isDebugOutput()) {
+				debugln("Time to get data from universe "+e.getKey()+": "+t2+"ms");
+			}
+
+			if (dmxData.length>0) {
+				debug(StatisticHelper.INSTANCE.getFrameCount()+" send "+dmxData.length+" bytes to universe "+currentUniverse);
+
+				byte[] data = Tpm2Protocol.doProtocol(dmxData, currentUniverse, cfg.getDmxToOffsetMap().size());
+				cfg.getOutput().sendData(currentUniverse, data);
+
+				StatisticHelper.INSTANCE.incrementAndGetPacketsRecieved();
+				StatisticHelper.INSTANCE.updateSendBytes(dmxData.length);
+				currentUniverse++;
+
+				debugln("... done");
+				if (System.currentTimeMillis()-lastDebugOutput > STATISTIC_OUTPUT_TIMEOUT) {
+					LOG.info(" sent "+StatisticHelper.INSTANCE.getPacketCount()+" packages ("+(StatisticHelper.INSTANCE.getSentBytes()/1024)+" kb), errors: "
+							+StatisticHelper.INSTANCE.getErrorCount());						
+					lastDebugOutput = System.currentTimeMillis();
+				}
+
+			} else {
+				debugln("no data found for universe "+e.getKey());
+				if (System.currentTimeMillis()-lastErrorMessage > WARNING_NO_DMX_DATA_TIMEOUT) {
+					LOG.info("no dmx data for universe "+e.getKey());						
+					lastErrorMessage = System.currentTimeMillis();
+				}
+			}
+		}
+
+		long cnt = StatisticHelper.INSTANCE.incrementAndGetFrameCount();
+		framerate.waitForFps(cnt);		
+	}
+
+	/**
+	 * 
+	 * @param s
+	 */
 	private static void debugln(String s) {
 		if (cfg.isDebugOutput()) {
 			System.out.println(s);
 		}		
 	}
 
+	/**
+	 * 
+	 * @param s
+	 */
 	private static void debug(String s) {
 		if (cfg.isDebugOutput()) {
 			System.out.print(s);
